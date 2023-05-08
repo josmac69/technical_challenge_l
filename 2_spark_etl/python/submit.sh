1afcfc38247cce018f4faf9c1110d7c505c9d840
Josef Machytka <josef.machytka@gmail.com>
2023-05-08 20:57:46 +0200

    removed unnecessary mkdir code problem was cause by using wrong spark image

--- 2_spark_etl/python/submit.sh
+++ 2_spark_etl/python/submit.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Create the cache directory and grant write permissions
-mkdir -p /home/spark/.ivy2/cache
-chmod -R 777 /home/spark/.ivy2/cache
-
 $SPARK_HOME/bin/spark-submit \
   --packages org.postgresql:postgresql:42.2.18 \
   --master local[*] \
